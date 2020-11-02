d6968131e56adda44891d3ca5f8870ac50834f83
Wayne <39239411+wayneswq@users.noreply.github.com>
2020-11-02 19:02:36 +0800

    Add script to start all cockroach nodes from local

---
+++ start-cockroach-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+startCockroach() {
+  echo "Starting cockroach on all 5 nodes"
+  yes "\n" | sshpass -p $1 ssh cs4224j@xcnc20.comp.nus.edu.sg "source .bash_profile; cd temp/; ./start-cockroach-node1.sh;" &
+  echo "node 1"
+  yes "\n" | sshpass -p $1 ssh cs4224j@xcnc21.comp.nus.edu.sg "source .bash_profile; cd temp/; ./start-cockroach-node2.sh;" &
+  echo "node 2"
+  yes "\n" | sshpass -p $1 ssh cs4224j@xcnc22.comp.nus.edu.sg "source .bash_profile; cd temp/; ./start-cockroach-node3.sh;" &
+  echo "node 3"
+  yes "\n" | sshpass -p $1 ssh cs4224j@xcnc23.comp.nus.edu.sg "source .bash_profile; cd temp/; ./start-cockroach-node4.sh;" &
+  echo "node 4"
+  yes "\n" | sshpass -p $1 ssh cs4224j@xcnc24.comp.nus.edu.sg "source .bash_profile; cd temp/; ./start-cockroach-node5.sh; cockroach init --insecure" &
+  echo "node 5"
+  echo "Cockroach initialised"
+}
+
+# ============================START=============================
+## $1: Password, $2 Number of clients: 20/40, $3 number of servers: 4/5
+echo "Starting cockroach on all 5 servers"
+startCockroach $1
