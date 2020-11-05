a94c840762b3bfbf5663e212eee358351990859d
ooihuiying <huang.ooihuiying@gmail.com>
2020-11-05 20:31:24 +0800

    undo commented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,14 +23,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        //int nodeID = Integer.parseInt(args[0]);
+        int nodeID = Integer.parseInt(args[0]);
 
-        //System.out.println("Running code on node " + nodeID);
+        System.out.println("Running code on node " + nodeID);
         // Configure the database connection.
         ds = new PGSimpleDataSource();
-         ds.setServerName("localhost"); //originally localhost
-         ds.setPortNumber(26257);
-        //ds.setUrl("jdbc:postgresql://" + IPAddresses.get(nodeID) + "/?sslmode=disable");
+         //ds.setServerName("localhost"); //originally localhost
+        // ds.setPortNumber(26257);
+        ds.setUrl("jdbc:postgresql://" + IPAddresses.get(nodeID) + "/?sslmode=disable");
 
         System.out.println(ds.getDescription());
         ds.setDatabaseName("cs4224"); // Impt
